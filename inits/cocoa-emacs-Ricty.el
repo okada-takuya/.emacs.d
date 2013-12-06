2fe8cee792ab9e39ebcab1e1cc7af9055cae7cb1
Takuya Okada <pitipitiunsyumikan@gmail.com>
2013-12-06 11:18:18 +0900

    Fix Ricty setting

--- inits/cocoa-emacs-Ricty.el
+++ inits/cocoa-emacs-Ricty.el
@@ -1,5 +1,5 @@
 ;; Fonts
-(let* ((size 15)
+(let* ((size 16)
        (asciifont "Ricty")
        (jpfont "Ricty")
        (h (* size 10))
@@ -7,7 +7,6 @@
        (jp-fontspec (font-spec :family jpfont)))
   (set-face-attribute 'default nil :family asciifont :height h)
   (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
-  (set-fontset-font nil 'japanese-j1isx0213-2 jp-fontspec)
   (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
-  (set-fontset-font nil '(#x0080 . #x024F) fontspec) 
+  (set-fontset-font nil '(#x0080 . #x024F) fontspec)
   (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
