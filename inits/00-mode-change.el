d04609b21dec6e5a2a651ad975cefe4719b56463
Takuya Okada <pitipitiunsyumikan@gmail.com>
2014-09-29 18:26:22 +0900

    Activate snippet

--- inits/00-mode-change.el
+++ inits/00-mode-change.el
@@ -3,6 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.watchr$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 ;; file contents
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
--- inits/10-yasnippet.el
+++ inits/10-yasnippet.el
@@ -1,1 +1,16 @@
-(require 'yasnippet)(setq yas-snippet-dirs      '("~/.emacs.d/snippets"        ))(yas-global-mode 1)(custom-set-variables '(yas-trigger-key "TAB"));; 既存スニペットを挿入する(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet);; 新規スニペットを作成するバッファを用意する(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet);; 既存スニペットを閲覧・編集する(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/mySnippets"
+        "~/.emacs.d/snippets"
+        ))
+
+(yas-global-mode 1)
+
+(custom-set-variables '(yas-trigger-key "TAB"))
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
