672bf3a75144628db1084cbb9f55c8a56e3d4933
Takuya Okada <pitipitiunsyumikan@gmail.com>
2018-12-18 23:31:20 +0900

    Add js2-mode setting

--- init.el
+++ init.el
@@ -20,6 +20,12 @@
 ;; 本来は (use-package hoge :straight t) のように書く必要がある
 (setq straight-use-package-by-default t)
 
+;; Add repositories
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 (package-initialize)
 
 ;; init-loader
